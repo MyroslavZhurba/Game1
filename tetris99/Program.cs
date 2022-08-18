2cc7da6d65ffc3de35f94933b11ad53097a2c832
Zhurba Myroslav <toparcher67@gmail.com>
2022-08-18 19:15:30 +0300

    start

---
+++ tetris99/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tetris99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
